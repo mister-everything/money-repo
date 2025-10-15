7cc51970eb0e53926dd2a39657e5431c8e19e50f
cgoing <neo.cgoing@gmail.com>
2025-10-15 15:15:21 +0900

    theme

--- apps/dash-board/components/layouts/site-header.tsx
+++ apps/dash-board/components/layouts/site-header.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export function SiteHeader() {
+  const { theme = "light", setTheme } = useTheme();
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -10,7 +16,17 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">DashBoard</h1>
+        <h1 className="text-base font-medium">Apps</h1>
+        <div className="ml-auto flex items-center gap-2">
+          <Button
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            variant="ghost"
+            size="icon"
+            className="hidden sm:flex"
+          >
+            {theme === "light" ? <SunIcon /> : <MoonIcon />}
+          </Button>
+        </div>
       </div>
     </header>
   );
