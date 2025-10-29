59ffd9db9732a4a0e9675f31edde34428dc3f108
hjcho <pj.hjcho@kalmate.net>
2025-10-29 09:00:17 +0900

    fix: review apply

--- services/solves/src/index.ts
+++ services/solves/src/index.ts
@@ -2,7 +2,5 @@ export * from "./payment/ai-price-admin.service";
 export * from "./payment/plan-admin.service";
 export * from "./payment/schema";
 export * from "./payment/subscription.service";
-export * from "./prob/blocks";
 export * from "./prob/prob.service";
 export * from "./prob/schema";
-export * from "./prob/types";
