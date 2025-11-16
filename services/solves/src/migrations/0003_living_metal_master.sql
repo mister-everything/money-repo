964d3e0d3e91fde4d0b47b7d0f5911680a449b75
joo-chang <jooc0311@gmail.com>
2025-11-16 18:06:02 +0900

    feat: db migrate 중복 에러 방지 추가

--- services/solves/src/migrations/0003_living_metal_master.sql
+++ services/solves/src/migrations/0003_living_metal_master.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "solves"."category_main" (
+CREATE TABLE IF NOT EXISTS "solves"."category_main" (
 	"category_main_id" serial PRIMARY KEY NOT NULL,
 	"code" varchar(20) NOT NULL,
 	"name" varchar(50) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE "solves"."category_main" (
 	CONSTRAINT "category_main_code_unique" UNIQUE("code")
 );
 --> statement-breakpoint
-CREATE TABLE "solves"."category_sub" (
+CREATE TABLE IF NOT EXISTS "solves"."category_sub" (
 	"category_sub_id" serial PRIMARY KEY NOT NULL,
 	"code" varchar(20) NOT NULL,
 	"name" varchar(50) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE "solves"."category_sub" (
 	CONSTRAINT "category_sub_code_unique" UNIQUE("code")
 );
 --> statement-breakpoint
-CREATE TABLE "solves"."prob_book_category" (
+CREATE TABLE IF NOT EXISTS "solves"."prob_book_category" (
 	"prob_book_id" uuid NOT NULL,
 	"category_main_id" integer NOT NULL,
 	"category_sub_id" integer NOT NULL,
@@ -23,7 +23,26 @@ CREATE TABLE "solves"."prob_book_category" (
 	CONSTRAINT "prob_book_category_prob_book_id_category_main_id_category_sub_id_pk" PRIMARY KEY("prob_book_id","category_main_id","category_sub_id")
 );
 --> statement-breakpoint
-ALTER TABLE "solves"."category_sub" ADD CONSTRAINT "category_sub_category_main_id_category_main_category_main_id_fk" FOREIGN KEY ("category_main_id") REFERENCES "solves"."category_main"("category_main_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "solves"."prob_book_category" ADD CONSTRAINT "prob_book_category_prob_book_id_prob_books_id_fk" FOREIGN KEY ("prob_book_id") REFERENCES "solves"."prob_books"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "solves"."prob_book_category" ADD CONSTRAINT "prob_book_category_category_main_id_category_main_category_main_id_fk" FOREIGN KEY ("category_main_id") REFERENCES "solves"."category_main"("category_main_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "solves"."category_sub" ADD CONSTRAINT "category_sub_category_main_id_category_main_category_main_id_fk" FOREIGN KEY ("category_main_id") REFERENCES "solves"."category_main"("category_main_id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+	WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "solves"."prob_book_category" ADD CONSTRAINT "prob_book_category_prob_book_id_prob_books_id_fk" FOREIGN KEY ("prob_book_id") REFERENCES "solves"."prob_books"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+	WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "solves"."prob_book_category" ADD CONSTRAINT "prob_book_category_category_main_id_category_main_category_main_id_fk" FOREIGN KEY ("category_main_id") REFERENCES "solves"."category_main"("category_main_id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+	WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
 ALTER TABLE "solves"."prob_book_category" ADD CONSTRAINT "prob_book_category_category_sub_id_category_sub_category_sub_id_fk" FOREIGN KEY ("category_sub_id") REFERENCES "solves"."category_sub"("category_sub_id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+	WHEN duplicate_object THEN null;
+END $$;
