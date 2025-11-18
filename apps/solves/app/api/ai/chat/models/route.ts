373924a34d1074f1a120aa25ee9b55011d035c40
cgoing <neo.cgoing@gmail.com>
2025-11-18 16:25:44 +0900

    cache

--- apps/solves/app/api/ai/chat/models/route.ts
+++ apps/solves/app/api/ai/chat/models/route.ts
@@ -1,13 +1,10 @@
 import { aiPriceService } from "@service/solves";
-import { TIME } from "@workspace/util";
 import { nextOk } from "@/lib/protocol/next-route-helper";
 
-export const revalidate = TIME.SECONDS(10);
+export const revalidate = 60 * 10;
 
 export async function GET() {
   const models = await aiPriceService.getActivePrices();
-
-  console.log(`!!!!current!`);
 
   return nextOk(
     models.map((model) => {
