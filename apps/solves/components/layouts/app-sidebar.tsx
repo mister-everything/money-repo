f0709eb6d1dd0499b14eda6ff41bbfc63c5f97db
joo-chang <jooc0311@gmail.com>
2025-11-22 18:12:02 +0900

    feat: sidebar 수정

--- apps/solves/components/layouts/app-sidebar.tsx
+++ apps/solves/components/layouts/app-sidebar.tsx
@@ -31,26 +31,25 @@ export function AppSidebar() {
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <SidebarLink href="/workbooks">문제 풀기</SidebarLink>
-        <SidebarMenuItem>
-          <SidebarMenuSub>
-            <SidebarMenuSubItem>
-              <SidebarMenuSubButton asChild>
-                <Link href="/workbooks/in-progress">풀고 있는 문제집</Link>
-              </SidebarMenuSubButton>
-            </SidebarMenuSubItem>
-            <SidebarMenuSubItem>
-              <SidebarMenuSubButton asChild>
-                <Link href="/workbooks/completed">다 푼 문제집</Link>
-              </SidebarMenuSubButton>
-            </SidebarMenuSubItem>
-          </SidebarMenuSub>
-        </SidebarMenuItem>
         <SidebarGroup>
-          <SidebarMenu>
-            <SidebarLink href="/workbooks">문제 풀기</SidebarLink>
-            <SidebarLink href="/workbooks/new">문제 생성</SidebarLink>
-          </SidebarMenu>
+          <SidebarLink href="/workbooks">문제 풀기</SidebarLink>
+          <SidebarMenuItem>
+            <SidebarMenuSub>
+              <SidebarMenuSubItem>
+                <SidebarMenuSubButton asChild>
+                  <Link href="/workbooks/in-progress">풀고 있는 문제집</Link>
+                </SidebarMenuSubButton>
+              </SidebarMenuSubItem>
+              <SidebarMenuSubItem>
+                <SidebarMenuSubButton asChild>
+                  <Link href="/workbooks/completed">다 푼 문제집</Link>
+                </SidebarMenuSubButton>
+              </SidebarMenuSubItem>
+            </SidebarMenuSub>
+          </SidebarMenuItem>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarLink href="/workbooks/new">문제 생성</SidebarLink>
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter className="flex flex-col items-stretch space-y-2">
