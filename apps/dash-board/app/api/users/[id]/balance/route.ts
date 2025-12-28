ebb4dc3c75cb4f61ad7acdaa9a25ebd0cf0a83b2
hjcho <bobob935@gmail.com>
2025-12-28 20:27:55 +0900

    feat: 유저에따른 기본 잔액 조회 2차

--- apps/dash-board/app/api/users/[id]/balance/route.ts
+++ apps/dash-board/app/api/users/[id]/balance/route.ts
@@ -1,11 +1,24 @@
+import { userService } from "@service/auth";
 import { walletService } from "@service/solves";
-import { nextOk } from "@/lib/protocol/next-route-helper";
+import { getSession } from "@/lib/auth/server";
+import { nextFail, nextOk } from "@/lib/protocol/next-route-helper";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
 ) {
+  // 관리자일 경우에만 접근 가능
+  const session = await getSession();
+  const isAdminCheck = await userService.isAdmin(session.user.id);
+  if (!isAdminCheck) {
+    return nextFail("Forbidden", 403);
+  }
+
   const { id } = await params;
   const wallet = await walletService.getWalletByUserId(id);
-  return nextOk(Number(wallet?.balance));
+  console.dir(wallet, { depth: null });
+  if (!wallet) {
+    return nextOk("not wallet");
+  }
+  return nextOk(Number(wallet.balance));
 }
