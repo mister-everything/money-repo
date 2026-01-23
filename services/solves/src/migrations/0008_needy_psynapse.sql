d8cf8312154ece93eaf6d325dabf604f3cc535f6
joo-chang <jooc0311@gmail.com>
2026-01-23 15:06:45 +0900

    chore: db migrate if 처리

--- services/solves/src/migrations/0008_needy_psynapse.sql
+++ services/solves/src/migrations/0008_needy_psynapse.sql
@@ -1,8 +1,13 @@
-CREATE TABLE "solves"."community_comment" (
+CREATE TABLE IF NOT EXISTS "solves"."community_comment" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" text NOT NULL,
 	"content" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+DO $$ BEGIN
 ALTER TABLE "solves"."community_comment" ADD CONSTRAINT "community_comment_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END $$;
+--> statement-breakpoint
